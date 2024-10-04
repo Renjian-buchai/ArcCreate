cf3a8e30780f38dfabf9f7288243b0d019a6465c
Segfault (Core dumped) <tyiwei0@gmail.com>
2024-10-04 21:04:03 +0800

    Dependencies: Added class to initialise Bass audio system

---
+++ Assets/Scripts/Gameplay/AudioSystem.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using ManagedBass;
+using UnityEngine;
+
+namespace ArcCreate.AudioSystem {
+  public static class AudioSystem {
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSplashScreen)]
+    public static void Initialise() {
+      // Currently here so that if something dies, at least it only takes down the hitsounds
+
+      // Code basically copied from https://github.com/2394425147/my-findings/
+
+      Bass.Init();
+
+      Application.quitting += Dispose;
+
+      Bass.UpdatePeriod = 5;
+      Bass.DeviceBufferLength = 10;
+      Bass.PlaybackBufferLength = 100;
+      Bass.DeviceNonStop = true;
+
+      // To synchronise audio with direct sound playback, if it ever falls back to that
+      Bass.Configure(Configuration.TruePlayPosition, 0);
+
+      // Set audio policy to one that obeys the mute switch
+      // Probably means that it turns off audio when silent mode is on
+      Bass.Configure(Configuration.IOSMixAudio, 5);
+
+      // Always provide a default device
+      Bass.Configure(Configuration.IncludeDefaultDevice, true);
+
+      // Enable custom BASS_CONFIG_MP3_OLDGOPS flag for backwards compatibility
+      Bass.Configure((Configuration)68, 1);
+
+      // Disable BASS_CONFIG_DEV_TIMEOUT flag to keep BASS audio output from pausing on device processing timeout.
+      // See https://www.un4seen.com/forum/?topic=19601 for more information.
+      Bass.Configure((Configuration)70, false);
+    }
+
+    public static void Dispose() {
+      Bass.Free();
+
+      Application.quitting -= Dispose;
+    }
+  }
+}
